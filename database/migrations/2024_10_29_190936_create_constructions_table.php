fadc3a381e3b464d4b82bd4a04fd1e65dfe95572
Miguel03br <167449074+Miguel03br@users.noreply.github.com>
2024-11-15 18:28:00 -0300

    Add files via upload

---
+++ database/migrations/2024_10_29_190936_create_constructions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('constructions', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->date('data_de_finalizacao');
+            $table->string('andamento_da_obra');
+            $table->string('solicitacao_de_materiais');
+            $table->string('relatorio_da_obra');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('constructions');
+    }
+};
